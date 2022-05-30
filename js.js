ed1b4b94af590bebec5a61897483ffb3987236eb
Gordon Gao <chenghaogordon@gmail.com>
2022-05-30 15:37:14 +1000

    as

---
+++ js.js
@@ -0,0 +1,11 @@
+
+function myfun() {
+   document.getElementById("demo").innerHTML = "Here i am";
+}
+
+function toCelsius(F) {
+   return (5/9)*(F-32)
+}
+function displayDate() {
+   document.getElementById('time').innerHTML=Date();
+}
